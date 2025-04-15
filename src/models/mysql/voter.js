835e8cd215464fdc8790c585b9b1d5595d8b8b20
Camilo Ramírez <camiloram18042003@gmail.com>
2025-04-15 14:35:29 -0300

    [UPDATE]: more correct variable name

--- src/models/mysql/voter.js
+++ src/models/mysql/voter.js
@@ -4,11 +4,11 @@ class VoterModel {
   static async getVoter ({ document }) {
     const connection = await createConnection()
     try {
-      const [voter] = await connection.query('SELECT * FROM voters WHERE document = ?', [document])
-      if (voter.length === 0) {
+      const [voters] = await connection.query('SELECT * FROM voters WHERE document = ?', [document])
+      if (voters.length === 0) {
         return null
       }
-      return voter[0]
+      return voters[0]
     } catch (error) {
       console.error('Error fetching voter:', error)
       throw error
